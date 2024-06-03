bd4c7357170f36885af1aad50ce5ee3c830f2285
lili_ana <lilianah33446@gmail.com>
2024-06-03 13:43:34 -0500

    Recepcion de mensaje y guardado de pdfs

--- index.js
+++ index.js
@@ -2,7 +2,64 @@ const { Client } = require('pg');
 const express = require('express');
 const axios = require('axios');
 const PDFDocument = require('pdfkit');
-const consumeMessages = require('./consumer')
+const fs = require('fs')
+require('dotenv').config()
+const jpeg = require('jpeg-js')
+
+const amqp = require('amqplib/callback_api');
+
+const consumeMessages = () => {
+    amqp.connect('amqp://192.168.0.102:5672', (err, connection) => {
+        if (err) {
+            console.error('Connection error:', err);
+            throw err;
+        }
+
+        connection.createChannel((err, channel) => {
+            if (err) {
+                console.error('Channel creation error:', err);
+                throw err;
+            }
+
+            const queue = 'tasks_queue';
+
+            channel.assertQueue(queue, {
+                durable: true,
+            });
+
+            // Prefetch to ensure only one message is handled at a time per consumer
+            channel.prefetch(1);
+
+            console.log('Waiting for messages in %s', queue);
+
+            channel.consume(queue, async (msg) => {
+                if (msg !== null) {
+                    try {
+                        const messageContent = JSON.parse(msg.content.toString());
+                        const { keyword, email } = messageContent;
+                        console.log(`Received: keyword=${keyword}, email=${email}`);
+
+                        // Simulating message processing, replace with your actual logic
+                        //await createReport(keyword);
+                        console.log(`procesando mensaje ${keyword}`)
+                        await createReport(keyword);
+
+                        channel.ack(msg);
+                        console.log(`Processed: keyword=${keyword}, email=${email}`);
+                    } catch (error) {
+                        console.error('Error processing message:', error);
+                        // Optional: channel.nack(msg, false, true); // requeue the message in case of error
+                    }
+                }
+            }, {
+                noAck: false, // Ensure acknowledgements are required
+            });
+        });
+    });
+};
+
+consumeMessages();
+
 
 const client = new Client({
     host: 'localhost',
@@ -25,6 +82,7 @@ const port = process.env.PORT;
 
 
 const createReport = async (keyword) => {
+    console.log(`Iniciando proceso de creacion de pdf, palabra recibida ${keyword}`)
     try {
 
         const buffers = await getAllImageBuffers(); // Obtener los buffers de las imágenes
@@ -36,13 +94,11 @@ const createReport = async (keyword) => {
         const result = await client.query(`SELECT * FROM users WHERE email LIKE '%${keyword}%' LIMIT ${arrowNum}`);
         console.log('Datos obtenidos de la DB exitosamente!');
 
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'attachment; filename=report.pdf');
-
-        await createPDF(res, result.rows, buffers); 
+        const filePath = `./report-${keyword}.pdf`;
+        await createPDF(filePath, result.rows, buffers);
+        console.log(`PDF guardado en ${filePath}`);
     } catch (error) {
         console.error('Error ejecutando la consulta', error.stack);
-        res.status(500).send(`Error ejecutando la consulta`); //para la palabra ${keyword}
     }
 }
 
@@ -67,13 +123,21 @@ const getImagesUrl = async () => {
 
 
 const getImgBytes = async (imageUrl) => {
-    console.log("Imagen recuperada", imageUrl)
     const maxRetries = 3;
     let attempts = 0;
     while (attempts < maxRetries) {
         try {
             const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-            return response.data; // Retorna los datos de la imagen
+            const buffer = Buffer.from(response.data);
+
+            // Verificar si el buffer es una imagen JPEG válida
+            const isValidJPEG = jpeg.decode(buffer, true);
+            if (!isValidJPEG) {
+                throw new Error('Invalid JPEG');
+            }
+
+            console.log("Buffer de imagen recuperado");
+            return buffer;
         } catch (error) {
             attempts++;
             console.error(`Error obteniendo los bytes de la imagen, intento ${attempts} de ${maxRetries}`, error.stack);
@@ -82,7 +146,7 @@ const getImgBytes = async (imageUrl) => {
             }
         }
     }
-}
+};
 
 // Función para obtener los buffers de todas las imágenes
 const getAllImageBuffers = async () => {
@@ -99,33 +163,45 @@ const getAllImageBuffers = async () => {
     }
 };
 
-const createPDF = async (res, data, buffers) => {
-    const doc = new PDFDocument();
 
-    doc.pipe(res);
+const createPDF = async (filePath, data, buffers) => {
+    try {
+        const doc = new PDFDocument();
+        const stream = fs.createWriteStream(filePath);
 
-    for (let i = 0; i < data.length; i++) {
-        const rowData = data[i];
-        const buffer = buffers[i];
+        doc.pipe(stream);
 
-        // Escribir datos de la fila
-        doc.text(JSON.stringify(rowData));
+        for (let i = 0; i < data.length; i++) {
+            const rowData = data[i];
+            const buffer = buffers[i];
 
-        // Insertar imagen
-        if (buffer) {
-            doc.image(buffer, { width: 200 });
-        } else {
-            doc.text("No se pudo obtener la imagen.");
+            // Escribir datos de la fila
+            doc.text(JSON.stringify(rowData));
+
+            // Insertar imagen
+            if (buffer) {
+                doc.image(buffer, { width: 200 });
+            } else {
+                doc.text("No se pudo obtener la imagen.");
+            }
+
+            // Agregar un salto de página después de cada fila
+            if (i !== data.length - 1) {
+                doc.addPage();
+            }
         }
 
-        // Agregar un salto de página después de cada fila
-        if (i !== data.length - 1) {
-            doc.addPage();
-        }
+        console.log('PDF creado')
+        doc.end();
+
+        // // Asegurarse de que el flujo se cierre correctamente
+        // await new Promise((resolve, reject) => {
+        //     stream.on('finish', resolve);
+        //     stream.on('error', reject);
+        // });
+    } catch (error) {
+        console.error('Error creando el pdf', error.stack);
     }
-
-    console.log('PDF creado')
-    doc.end();
 }
 
 
